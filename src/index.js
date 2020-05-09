2ed4107ab41446b8903580d64beb909b47243fee
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-05-09 11:08:21 -0300

    começo da montagem do websocket

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ require('dotenv/config');
 const { errors } = require('celebrate');
 const routes = require('./routes');
 const path = require('path');
+const http = require('http');
 
 global.__basedir = __dirname;
 
@@ -16,4 +17,19 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')
 app.use(routes);
 app.use(errors());
 
-app.listen(process.env.PORT || 3001);
+
+const server = http.createServer(app);
+const io = require('socket.io').listen(server);
+
+io.on('connection', socket => {
+	console.log('[IO] Connection => Server has a new connection');
+	socket.on('chat.message', data => {
+		console.log('[SOCKET] Chat.message => ', data);
+		io.emit('chat.message', data);
+	});
+	socket.on('disconnect', () => {
+		console.log('[SOCKET] Disconnect => A connection was disconnected');
+	});
+});
+
+server.listen(process.env.PORT || 3001);
